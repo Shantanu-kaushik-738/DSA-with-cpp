8597ac45b1ebb5c8b23b70b60c13bbfeeb45f3d1
Shantanu-kaushik-738 <kaushikshantanu420@gmail.com>
2026-07-24 10:21:46 +0530

    Deque data structure

---
+++ stack_&_queue/deque.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    deque<int> dq;
+
+    dq.push_back(1);
+    dq.push_back(2);
+    dq.push_back(3);
+    dq.push_front(4);
+
+    cout << dq.front() << " " << dq.back() << endl;
+
+    return 0;
+}
