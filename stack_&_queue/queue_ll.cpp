3fc24b0cda9be8348fc8d3f89bb38fc369c13d91
Shantanu-kaushik-738 <kaushikshantanu420@gmail.com>
2026-07-24 10:13:00 +0530

    Queue using linked list

---
+++ stack_&_queue/queue_ll.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int v)
+    {
+        data = v;
+        next = NULL;
+    }
+};
+
+class Queue
+{ // using linked list
+    Node *head;
+    Node *tail;
+
+public:
+    Queue()
+    {
+        head = tail = NULL;
+    }
+
+    int push(int i)
+    { // insert at tail
+        Node *newNode = new Node(i);
+
+        if (empty())
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void pop()
+    {
+        if (empty())
+        {
+            cout << "LL is empty" << endl;
+            return;
+        }
+
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    int front()
+    {
+        if (empty())
+        {
+            cout << "LL is empty" << endl;
+            return -1;
+        }
+        return head->data;
+    }
+
+    bool empty()
+    {
+        return head == NULL;
+    }
+};
+
+int main()
+{
+    Queue q;
+
+    q.push(1);
+    q.push(2);
+    q.push(3);
+
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
